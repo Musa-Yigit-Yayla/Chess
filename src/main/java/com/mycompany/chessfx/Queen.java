6f8bcdc60be0f50064acc5f303745239fedc560f
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-10 15:52:05 +0300

    Queen's showMoveables implemented
    Implemented by using the showMoveables functions of rook and bishop.

--- src/main/java/com/mycompany/chessfx/Queen.java
+++ src/main/java/com/mycompany/chessfx/Queen.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.chessfx;
 
+import java.util.Arrays;
 /**
  *
  * @author yigit
@@ -24,12 +25,43 @@ public class Queen extends Piece{
     public void move() {
        
     }
-    public void castle(){
-        
-    }
-
+    
+    //Queen behaves as a combination of a bishop and a rook, so we will use their corresponding showMoveables methods by instantiating
+    //a friendly bishop and a rook that is outside the game
+    //Later we will leave these new objects to jvm garbage collection
     @Override
     public Object[] showMoveables() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        final int row = super.getRow();
+        final int column = super.getColumn();
+        
+        String friendlyColor = this.getColor();
+        String enemyColor = super.getEnemyColor();
+        
+        char color;
+        //instantiate a bishop and a rook of the same color independent from our game
+        if(friendlyColor.equals(Piece.WHITE_COLOR)){
+            color = 'w';
+        }
+        else{
+            color = 'b';
+        }
+        Bishop b = new Bishop(color);
+        Rook r = new Rook(color);
+        
+        b.setPosition(row, column);
+        r.setPosition(row, column);
+        
+        Object[] arr1 = (String[])b.showMoveables();
+        Object[] arr2 = (String[])r.showMoveables();
+        
+        //concatenate the two arrays
+        Object[] result = Arrays.copyOf(arr1, arr1.length + arr2.length);
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+        
+        //Explicitly assign null to instantiated helper objects
+        b = null;
+        r = null;
+        
+        return result;
     }
 }
