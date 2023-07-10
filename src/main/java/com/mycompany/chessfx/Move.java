d168232e950de971ff40f4d48de598cd2d76a742
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2023-07-10 16:16:34 +0300

    Move constructor provided

--- src/main/java/com/mycompany/chessfx/Move.java
+++ src/main/java/com/mycompany/chessfx/Move.java
@@ -16,5 +16,25 @@ public class Move {
     private static Move lastMove; // last valid move that has been made
     private static int turn = WHITE_TURN;// white's turn initially
     
+    private Piece piece;
+    private String prevPos;
+    private String newPos;
     
+    public Move(Piece piece, String prevPos, String newPos){
+        this.piece = piece;
+        this.prevPos = prevPos;
+        this.newPos = newPos;
+        
+        //alter the static variable turn since a new move has been made
+        turn *= -1;
+        //set the lastMove to this object
+        lastMove = this;
+    }
+    public Move getLastMove(){
+        return lastMove;
+    }
+    //1 for white turn, -1 for black turn
+    public int getTurn(){
+        return turn;
+    }
 }
